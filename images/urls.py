70d340318f0600065b4a87b4302157353bc015e4
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-05-29 11:55:35 +0300

    ft: add search functionality by category

--- images/urls.py
+++ images/urls.py
@@ -4,7 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^search/', views.search_results, name='search_results')
+
 ]
 
 if settings.DEBUG:
--- images/views.py
+++ images/views.py
@@ -8,3 +8,14 @@ def index(request):
     images = Images.get_image()
     return render(request, 'index.html', {"images": images})
 
+def search_results(request):
+    if 'Image' in request.GET and request.GET["Image"]:
+        search_term = request.GET.get("Image")
+        searched_images = Images.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html', {"message": message, "images": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html', {"message": message})
