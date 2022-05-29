6b3726457bfde85d94bd03b3b5c9b201ba36f379
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-05-29 11:22:25 +0300

    ft: display images from database

--- images/views.py
+++ images/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import Http404, HttpResponse
+from .models import Images
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    images = Images.get_image()
+    return render(request, 'index.html', {"images": images})
 
