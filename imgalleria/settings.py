bb481818d6fe684c5a350dba7142ac6b9c2dd8aa
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-05-30 09:14:41 +0300

    fix: duplicate settings

--- imgalleria/settings.py
+++ imgalleria/settings.py
@@ -144,10 +144,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
